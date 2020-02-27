58c7ee73fdeb9d1f0053660e9a5f953956027c58
Jeanju You <jyou5@student.gsu.edu>
2020-02-27 00:18:55 -0500

    Create task object class

---
+++ src/main/java/edu/gsu/bbb/willdo/Task.java
@@ -0,0 +1,44 @@
+package edu.gsu.bbb.willdo;
+
+import org.springframework.data.annotation.idI;
+
+public class Task{
+    @Id private int id;
+
+    private String summary;
+    private String description;
+    private String date;
+    private boolean state;
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public boolean isState() {
+        return state;
+    }
+
+    public void setState(boolean state) {
+        this.state = state;
+    }
+}
