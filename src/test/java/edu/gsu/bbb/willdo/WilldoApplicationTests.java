4ec4cb3251a0176edb6af720cf92c7ed996c6c12
Jeanju You <jyou5@student.gsu.edu>
2020-02-26 21:14:29 -0500

    Initial commit -skeleton spring project -spring initializr     +Maven, Java, 2.2.4, no dependencies

---
+++ src/test/java/edu/gsu/bbb/willdo/WilldoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.gsu.bbb.willdo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WilldoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
