982ee5925385da30c846041605d3dd639967a8e6
Jeanju You <jyou5@student.gsu.edu>
2020-02-29 01:24:26 -0500

    Add logic for error response handling Actual code to handle error response logic not added yet

--- src/main/java/edu/gsu/bbb/willdo/TaskController.java
+++ src/main/java/edu/gsu/bbb/willdo/TaskController.java
@@ -19,12 +19,31 @@ public class TaskController {
 
     @GetMapping("/tasks/{id}")
     public Optional<Task> task(@PathVariable String id) {
+        Optional<Task> findTask = repository.findById(id);
+        Optional<Task> empty = Optional.empty(); //to see if it leaves loop
 
-        return repository.findById(id);
+        if(!findTask.isPresent()) {
+            //some response annotation; invalid parameters?
+        } else {
+            return findTask;
+        }
+        return empty; //should not return this ever
     }
 
     @PostMapping("/tasks")
-    public Task newTask(@RequestBody Task newTask) {
-        return repository.save(newTask);
+    public Object newTask(@RequestBody Task newTask) {
+        Optional<Task> empty = Optional.empty(); //to see if it leaves loop
+
+        if(newTask.getSummary() == null){
+            //some response annotation; null values
+        } else {
+            return repository.save(newTask);
+        }
+        return empty; //should not return this ever
     }
+
+//    @PutMapping("/tasks/{id}")
+//    public Task updateTask(@RequestBody Task newTask, @PathVariable String id) {
+//
+//    }
 }
