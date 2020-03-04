107cc4fe62401f40de55e3bc7e26282b13ae1ce7
Ned Anderson <NedAnd1@users.noreply.github.com>
2020-03-03 20:02:07 -0500

    more robust scripts for unix & windows

--- client/RUN.cmd
+++ client/RUN.cmd
@@ -3,8 +3,6 @@ echo Note: Node.js needs to be installed prior to running this script
 
 cd /d %~dp0
 
-IF NOT EXIST node_modules\.bin\ng.cmd (
-	npm install
-)
+IF NOT EXIST node_modules\.bin\ng.cmd call npm install
 
 node_modules\.bin\ng.cmd serve
--- runall.cmd
+++ runall.cmd
@@ -4,20 +4,19 @@ echo Note: Node.js, JDK, and MongoDB need to be installed prior to running this 
 
 
 :: make sure the database is running
-@start /b cmd /c mongod
+start /b cmd /c mongod
 
 
 :: make sure the server is running
 cd %~dp0
-@start /b cmd /c mvnw spring-boot:run
+start /b cmd /c mvnw.cmd spring-boot:run
 
 
 :: make sure the client is running
 cd client
-IF NOT EXIST node_modules/.bin/ng.cmd (
-	npm install
-)
-@start /b cmd /c node_modules\.bin\ng.cmd serve
+IF NOT EXIST node_modules\.bin\ng.cmd call npm install
+
+start /b cmd /c node_modules\.bin\ng.cmd serve
 
 
 :: open the web page
