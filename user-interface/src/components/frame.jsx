4a0ac57e70bbdd6a86e7561e141ff2a69c050567
Warigari <warigaricoding@gmail.com>
2020-03-09 21:04:59 -0400

    made buttons

---
+++ user-interface/src/components/frame.jsx
@@ -0,0 +1,16 @@
+import React, {Component } from 'react';
+
+export default class Frame extends Component {
+    state={}
+    render() {
+        return (
+        <div>
+        <h1>WillDo</h1>
+        <button>Group1</button>
+        <h6>//</h6>
+        <button>Group2</button>
+        </div>
+        );
+    }
+
+}
