3c0d6728c75d3efb3030c0d1c27d2b8397714282
Samuel Fekadu <samft99@gmail.com>
2020-03-30 20:42:08 -0400

    Remove Front End from Back end Branch
    +Remove misplaced commits and clean Branch

--- client/RUN.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Note: Node.js needs to be installed prior to running this script
-
-cd /d %~dp0
-
-IF NOT EXIST node_modules\.bin\ng.cmd call npm install
-
-node_modules\.bin\ng.cmd serve
